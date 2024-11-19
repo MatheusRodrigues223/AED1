a06f1117421ce9ea7ee3ecd387fd2ae3285c1841
Matheus Rodrigues <168884771+MatheusRodrigues223@users.noreply.github.com>
2024-11-19 14:19:18 -0300

    Create 2374.c

---
+++ 2374.c
@@ -0,0 +1,19 @@
+// Matheus da Rosa Rodrigues
+// RA: 176614
+
+#include <stdio.h>
+ 
+int main() {
+    int N, M;
+    
+    scanf("%d", &N);
+    getchar();
+    
+    scanf("%d", &M);
+    getchar();  
+
+    
+    printf("%d\n", N - M);
+
+    return 0;
+}
