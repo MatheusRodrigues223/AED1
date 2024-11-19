45acd5710025e487300bee4ce8398c194f818eb4
Matheus Rodrigues <168884771+MatheusRodrigues223@users.noreply.github.com>
2024-11-19 14:16:02 -0300

    Create 2413.c

---
+++ 2413.c
@@ -0,0 +1,15 @@
+// Matheus da Rosa Rodrigues
+// RA: 176614
+
+#include <stdio.h>
+ 
+int main() {
+    int t;
+    
+    scanf("%d", &t);
+    getchar();
+    
+    printf("%d\n", 4*t);
+ 
+    return 0;
+}
