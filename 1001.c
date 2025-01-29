ff28f31b9af617b3bcd9d1bc4bb205664a350cde
Matheus Rodrigues <168884771+MatheusRodrigues223@users.noreply.github.com>
2025-01-29 20:21:56 -0300

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,16 @@
+// Matheus da Rosa Rodrigues
+// RA: 176614
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    printf("X = %d\n", A + B);
+ 
+    return 0;
+}
